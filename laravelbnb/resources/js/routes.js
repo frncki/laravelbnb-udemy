b64250fbc05e453afe0bacc56393e03940b35d09
Franek <panczo.mirecki@gmail.com>
2020-10-17 15:25:05 +0200

    init

---
+++ laravelbnb/resources/js/routes.js
@@ -0,0 +1,38 @@
+import VueRouter from "vue-router";
+import Bookables from "./bookables/Bookables";
+import Bookable from "./bookable/Bookable";
+import Review from "./review/Review";
+import Basket from "./basket/Basket";
+
+const routes = [
+    {
+        path: "/",
+        component: Bookables,
+        name: "home"
+    },
+
+    {
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable"
+    },
+
+    {
+        path: "/review/:id",
+        component: Review,
+        name: "review"
+    },
+
+    {
+        path: "/basket",
+        component: Basket,
+        name: "basket"
+    }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes // short for 'routes: routes'
+});
+
+export default router;
---
+++ laravelbnb/resources/js/shared/components/Success.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center success">
+    <i class="far fa-hand-spock"></i>
+    <h1 class="mt-4">Congrats</h1>
+    <h2>
+      <slot></slot>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.success {
+  color: #999;
+  font-size: 10rem;
+}
+</style>
